5063afeae072b5e1aa950874ccbdac61e74a4eec
AndreyTroitskiy <TroitskiyA@yandex.ru>
2026-02-09 00:15:50 +0700

    feat: cache

--- src/services/citiesService.js
+++ src/services/citiesService.js
@@ -2,8 +2,12 @@ const { ObjectId } = require("mongodb");
 const dataService = require("./mongodb");
 const eventsService = require("./eventsService");
 
+const citiesCache = [];
 
 async function getCities() {
+    if(citiesCache.length){
+        return citiesCache;
+    }
     const cities = await dataService.getDocuments('city', {});
     const events = await eventsService.getEvents();
     const eventsMap = events.reduce((map, event) => {
@@ -12,7 +16,8 @@ async function getCities() {
         map.set(event.city, events);
         return map
     }, new Map());
-    cities.forEach(city => city.events = eventsMap.get(city.id))
+    cities.forEach(city => city.events = eventsMap.get(city.id));
+    citiesCache.push(...cities)
     return cities;
 }
 
--- src/services/eventsService.js
+++ src/services/eventsService.js
@@ -2,13 +2,18 @@ const { ObjectId } = require("mongodb");
 const dataService = require("./mongodb");
 const socketService = require("./socketService");
 
+const eventsCache = [];
 
 async function getEventsByCity(city) {
-    const events =  await dataService.getDocuments('event', {city});
-    return events;
+    const events =  await getEvents();
+    return events.filter(event => event.city === city);
 }
 async function getEvents() {
+    if( eventsCache.length){
+        return eventsCache;
+    }
     const events =  await dataService.getDocuments('event', {});
+    eventsCache.push(...events);
     return events;
 }
 
