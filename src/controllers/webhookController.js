6d46f4a2e212e888641e76ac4d0938fe63ac6416
AndreyTroitskiy <TroitskiyA@yandex.ru>
2026-02-10 11:03:33 +0700

    feat: state

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -7,6 +7,8 @@ const axios = require("axios");
 const { ObjectId } = require("mongodb");
 const { sendMessage } = require("../services/messageService");
 
+const stateMap = new Map()
+
 const handleWebhook = async (req, res) => {
   try {
     const update = req.body;
@@ -27,6 +29,7 @@ const handleWebhook = async (req, res) => {
       const userId = cq.from.id.toString()
       isAdmin = config.admins.includes(userId)
       let text = cq.message.text + "\u200B";
+      let newPhoto;
       if (data === 'getCities') {
         const cities = await citiesService.getCities();
         reply_markup.inline_keyboard = cities.map(city => [
@@ -37,7 +40,7 @@ const handleWebhook = async (req, res) => {
         ])
         text = "Текст про список городов";
       } else {
-        const [action, value, context, stateStr] = data.split('_');
+        const [action, value, context] = data.split('_');
         switch (action) {
           case 'CITY': {
             const events = await eventsService.getEventsByCity(value);
@@ -52,16 +55,18 @@ const handleWebhook = async (req, res) => {
           }
           case 'EVENT': {
             const event = await eventsService.getEvent(value);
-            const state = stateStr ? stateStr.split(',') : event.tickets.map(() => 0);
-            state.push(0, 0)
+            const state = {
+              event: value,
+            }
+            stateMap.set(userId, state);
             reply_markup.inline_keyboard = event.tickets.reduce((rows, ticket) => {
               rows.push([
                 { text: `${config.ticketTypes[ticket.type.toString()] || 'Какой-то билет'}, ${ticket.priceVND}.000 VND/${ticket.priceRub} руб`, callback_data: `TICKET_${ticket.type}` }
               ])
               rows.push([
-                { text: '➖', callback_data: `DECR_${value}_${ticket.type}_${state.join(',')}` },
-                { text: state[ticket.type], callback_data: "NOTHING" },
-                { text: '➕', callback_data: `INCR_${value}_${ticket.type}_${state.join(',')}` }
+                { text: '➖', callback_data: `DECR_${value}_${ticket.type}` },
+                { text: 0, callback_data: "NOTHING" },
+                { text: '➕', callback_data: `INCR_${value}_${ticket.type}` }
               ])
               return rows;
             }, [])
@@ -74,27 +79,29 @@ const handleWebhook = async (req, res) => {
           }
           case 'INCR': {
             const event = await eventsService.getEvent(value);
-            const state = stateStr ? stateStr.split(',').map(Number) : event.tickets.map(() => 0);
-            const eventType = Number(context);
-            state[eventType] = state[eventType] + 1;
-            reply_markup.inline_keyboard.filter(row => row.length === 3).forEach((row, index) => {
-              row[0].callback_data = `DECR_${value}_${index}_${state.join(',')}`;
-              row[1].text = state[index];
-              row[2].callback_data = `INCR_${value}_${index}_${state.join(',')}`;
-            });
+            const state = stateMap.get(userId);
+            state[context] = (state[context] || 0) + 1;
+            let i = 0
+            for (const row of reply_markup.inline_keyboard) {
+              if(row => row.length === 3){
+                i += 2;
+                const [action, value, ticketType] = row[0].callback_data.split('_');
+                row[1].text = state[ticketType] || 0
+              }
+            }
             const backButton = reply_markup.inline_keyboard[reply_markup.inline_keyboard.length - 1];
             const [totalVND, totalRub] = event.tickets.reduce((res, ticket) => {
               res[0] += state[ticket.type] * ticket.priceVND;
               res[1] += state[ticket.type] * ticket.priceRub;
               return res;
             }, [0, 0]);
-            reply_markup.inline_keyboard.length = event.tickets.length * 2;
+            reply_markup.inline_keyboard.length = i;
             if (totalVND > 0) {
               reply_markup.inline_keyboard.push([
-                { text: `Купить за ${totalVND}.000 VND`, callback_data: `VND_${value}_${totalVND}_${state.join(',')}` },
+                { text: `Купить за ${totalVND}.000 VND`, callback_data: `VND_${value}_${totalVND}` },
               ])
               reply_markup.inline_keyboard.push([
-                { text: `Купить за ${totalRub} руб`, callback_data: `RUB_${value}_${totalRub}_${state.join(',')}` },
+                { text: `Купить за ${totalRub} руб`, callback_data: `RUB_${value}_${totalRub}` },
               ])
             }
             reply_markup.inline_keyboard.push(backButton)
@@ -103,29 +110,33 @@ const handleWebhook = async (req, res) => {
           }
           case 'DECR': {
             const event = await eventsService.getEvent(value);
-            const state = stateStr ? stateStr.split(',').map(Number) : event.tickets.map(() => 0);
-            const eventType = Number(context);
-            if (state[eventType] > 0) {
-              state[eventType] = state[eventType] - 1;
+            const state = stateMap.get(userId);
+            if (!state[context]) {
+              emptyButton = true;
+              break;
             }
-            reply_markup.inline_keyboard.filter(row => row.length === 3).forEach((row, index) => {
-              row[0].callback_data = `DECR_${value}_${index}_${state.join(',')}`;
-              row[1].text = state[index];
-              row[2].callback_data = `INCR_${value}_${index}_${state.join(',')}`;
-            });
+            state[context] = state[context] - 1;
+            let i = 0
+            for (const row of reply_markup.inline_keyboard) {
+              if(row => row.length === 3){
+                i += 2;
+                const [action, value, ticketType] = row[0].callback_data.split('_');
+                row[1].text = state[ticketType] || 0
+              }
+            }
             const backButton = reply_markup.inline_keyboard[reply_markup.inline_keyboard.length - 1];
             const [totalVND, totalRub] = event.tickets.reduce((res, ticket) => {
-              res[0] += state[ticket.type] * ticket.priceVND;
-              res[1] += state[ticket.type] * ticket.priceRub;
+              res[0] += state[ticket.type.toString()] * ticket.priceVND;
+              res[1] += state[ticket.type.toString()] * ticket.priceRub;
               return res;
             }, [0, 0]);
             reply_markup.inline_keyboard.length = event.tickets.length * 2;
             if (totalVND > 0) {
               reply_markup.inline_keyboard.push([
-                { text: `Купить за ${totalVND}.000 VND`, callback_data: `VND_${value}_${totalVND}_${state.join(',')}` },
+                { text: `Купить за ${totalVND}.000 VND`, callback_data: `VND_${value}_${totalVND}` },
               ])
               reply_markup.inline_keyboard.push([
-                { text: `Купить за ${totalRub} руб`, callback_data: `RUB_${value}_${totalRub}_${state.join(',')}` },
+                { text: `Купить за ${totalRub} руб`, callback_data: `RUB_${value}_${totalRub}` },
               ])
             }
             reply_markup.inline_keyboard.push(backButton)
@@ -134,7 +145,7 @@ const handleWebhook = async (req, res) => {
           }
           case 'VND': {
             const event = await eventsService.getEvent(value);
-            const state = stateStr ? stateStr.split(',').map(Number) : event.tickets.map(() => 0);
+            const state = stateMap.get(userId);
             const amount = Number(context);
             emptyButton = true;
             await axios.post(`${config.tgApiUrl}/sendPhoto`, {
@@ -143,8 +154,8 @@ const handleWebhook = async (req, res) => {
               caption: `Оплатите ${amount}.000 VND по этому QR, пришлите скрин квитанции, нажмите "Оплатил"`,
               reply_markup: {
                 inline_keyboard: [
-                  [{ text: `Оплатил`, callback_data: `PAYED_${value}_VND_${state.join(',')}` }],
-                  [{ text: `Назад`, callback_data: `CANCEL-PAYMENT` }],
+                  [{ text: `Оплатил`, callback_data: `PAYED_${value}_VND` }],
+                  [{ text: `Назад`, callback_data: `EVENT_${value}_${event.city}` }],
                 ]
               },
             });
@@ -161,18 +172,10 @@ const handleWebhook = async (req, res) => {
               caption: `Оплатите ${amount} руб. по по номеру 8-912-669-7190, пришлите скрин квитанции, нажмите "Оплатил"`,
               reply_markup: {
                 inline_keyboard: [
-                  [{ text: `Оплатил`, callback_data: `PAYED_${value}_RUB_${state.join(',')}` }],
-                  [{ text: `Назад`, callback_data: `CANCEL-PAYMENT` }],
+                  [{ text: `Оплатил`, callback_data: `PAYED_${value}_RUB` }],
+                  [{ text: `Назад`, callback_data: `EVENT_${value}_${event.city}` }],
                 ]
               },
-            });
-            break;
-          }
-          case 'CANCEL-PAYMENT': {
-            emptyButton = true;
-            await axios.post(`${config.tgApiUrl}/deleteMessage`, {
-              chat_id,
-              message_id: cq.message.message_id,
             });
             break;
           }
@@ -191,12 +194,26 @@ const handleWebhook = async (req, res) => {
         }
       }
       if (!emptyButton) {
-        await axios.post(`${config.tgApiUrl}/editMessageText`, {
-          chat_id,
-          message_id: cq.message.message_id,
-          text,
-          reply_markup,
-        });
+        if (newPhoto) {
+          await axios.post(`${config.tgApiUrl}/editMessageMedia`, {
+            chat_id,
+            message_id: cq.message.message_id,
+            media: {
+              type: 'photo',
+              media: newPhoto,
+              caption: text
+            },
+            reply_markup,
+          });
+
+        } else {
+          await axios.post(`${config.tgApiUrl}/editMessageCaption`, {
+            chat_id,
+            message_id: cq.message.message_id,
+            caption: text,
+            reply_markup,
+          });
+        }
       }
 
       await axios.post(`${config.tgApiUrl}/answerCallbackQuery`, {
@@ -208,27 +225,23 @@ const handleWebhook = async (req, res) => {
     }
     const message = update.message
     if (message && message.text === "/start") {
-      await fetch(`${config.tgApiUrl}/sendMessage`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          chat_id: message.chat.id,
-          text: "Добро пожаловать!",
-          reply_markup: {
-            inline_keyboard: [
-              [
-                { text: "Список городов", callback_data: "getCities" },
-              ]
+      await axios.post(`${config.tgApiUrl}/sendPhoto`, {
+        chat_id: message.chat.id,
+        photo: 'https://www.dropbox.com/scl/fi/2mg82u8ijul2lypcrjg2f/476246033_17959642448890365_3285800817416688546_n.jpg?rlkey=5jz9kq568fshixcnzb1la2fpz&dl=0',
+        caption: "Добро пожаловать!",
+        reply_markup: {
+          inline_keyboard: [
+            [
+              { text: "Список городов", callback_data: "getCities" },
             ]
-          }
-        })
+          ]
+        },
       });
-
     }
     if (message && message.photo) {
       await axios.post(`${config.tgApiUrl}/forwardMessage`, {
         chat_id: config.cashier,
-        from_chat_id: chat_id,
+        from_chat_id: message.chat.id,
         message_id: message.message_id
       });
     }
