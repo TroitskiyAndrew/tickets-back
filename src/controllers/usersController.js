acea1a03d4103077f48b24e6ec715b11b7899e80
AndreyTroitskiy <TroitskiyA@yandex.ru>
2026-03-06 16:48:31 +0700

    feat: spam

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -89,41 +89,38 @@ const sendMessage = async (req, res) => {
 
     // const users = await dataService.aggregate('user', [
     //   {
+    //     $match: {
+    //       visits: "6984920343c748577e5f8704"
+    //     }
+    //   },
+    //   {
     //     $lookup: {
     //       from: "ticket",
-    //       let: { userId: "$userId" },
+    //       let: { uid: "$userId" },
     //       pipeline: [
     //         {
     //           $match: {
     //             $expr: {
     //               $and: [
-    //                 { $eq: ["$userId", "$$userId"] },
-    //                 {
-    //                   $in: [
-    //                     "$event",
-    //                     [
-    //                       "6985e0b63677bfc5bc8757c3",
-    //                       "6985e0b63677bfc5bc8757c4"
-    //                     ]
-    //                   ]
-    //                 }
+    //                 { $eq: ["$userId", "$$uid"] },
+    //                 { $eq: ["$event", "6985e0b63677bfc5bc8757c3"] }
     //               ]
     //             }
     //           }
     //         },
-    //         { $limit: 1 } // достаточно одного билета
+    //         { $limit: 1 }
     //       ],
-    //       as: "matchedTickets"
+    //       as: "tickets"
     //     }
     //   },
     //   {
     //     $match: {
-    //       matchedTickets: { $ne: [] }
+    //       tickets: { $eq: [] }
     //     }
     //   },
     //   {
     //     $project: {
-    //       matchedTickets: 0
+    //       tickets: 0
     //     }
     //   }
     // ])
@@ -137,15 +134,12 @@ const sendMessage = async (req, res) => {
     //     await axios.post(`${config.tgApiUrl}/sendPhoto`, {
     //       chat_id: id,
     //       parse_mode: 'HTML',
-    //       photo: 'https://www.dropbox.com/scl/fi/pfxe9l923hal1imq5lhq9/what.jpg?rlkey=9vk13epfpfnont2jcjq90z8oi&raw=1',
-    //       caption: `Я к тебе с хорошей новостью. Мы снизили цены на билеты\nЕсли хочешь - вернем тебе часть денег или оформим депозит на баре 😉\nПлюс, персональная скидка 30% на второе шоу\nнапиши в чат с ботом, чтобы связаться с нами`,
+    //       photo: 'https://www.dropbox.com/scl/fi/apchcqxxw0q07cnj4ht4x/photo_2026-03-03_01-02-40.jpg?rlkey=rql3yov33n9xsz7age9y8gvlf&raw=1',
+    //       caption: `Чего ждем? Стендап концерт в Муйне уже сегодня, бери билеты пока не разобрали!`,
     //       reply_markup: {
     //         inline_keyboard: [
     //           [
-    //             { text: "Новые цены на Стендап-концерт 06.03 20:00", url: 'https://t.me/sverlov_vietnam_2026_bot?startapp=SOURCE_SPLIT_PRICE-LOW-NOTICE_SEP_EVENT_SPLIT_6985e0b63677bfc5bc8757c3' },
-    //           ],
-    //           [
-    //             { text: "Новые цены на Шоу-Импровизация 08.03 20:00", url: 'https://t.me/sverlov_vietnam_2026_bot?startapp=SOURCE_SPLIT_PRICE-LOW-NOTICE_SEP_EVENT_SPLIT_6985e0b63677bfc5bc8757c4' },
+    //             { text: "Посмотреть билеты", url: 'https://t.me/sverlov_vietnam_2026_bot?startapp=SOURCE_SPLIT_MUINE-RECALL_SEP_EVENT_SPLIT_6985e0b63677bfc5bc8757c3' },
     //           ]
     //         ]
     //       },
@@ -158,145 +152,8 @@ const sendMessage = async (req, res) => {
     //     fail.push(id)
     //   }
     // }
-
-    //     const users = await dataService.aggregate('user', [
-    //   {
-    //     // 1️⃣ Сначала отфильтруем по visits (использует индекс)
-    //     $match: {
-    //       visits: "6984920343c748577e5f8704"
-    //     }
-    //   },
-    //   {
-    //     // 2️⃣ Проверяем отсутствие нужных билетов
-    //     $lookup: {
-    //       from: "ticket",
-    //       let: { userId: "$userId" },
-    //       pipeline: [
-    //         {
-    //           $match: {
-    //             $expr: {
-    //               $and: [
-    //                 { $eq: ["$userId", "$$userId"] },
-    //                 {
-    //                   $in: [
-    //                     "$event",
-    //                     [
-    //                       "6985e0b63677bfc5bc8757c3",
-    //                       "6985e0b63677bfc5bc8757c4"
-    //                     ]
-    //                   ]
-    //                 }
-    //               ]
-    //             }
-    //           }
-    //         },
-    //         { $limit: 1 } // достаточно одного совпадения
-    //       ],
-    //       as: "matchedTickets"
-    //     }
-    //   },
-    //   {
-    //     // 3️⃣ Оставляем только тех, у кого таких билетов нет
-    //     $match: {
-    //       matchedTickets: { $eq: [] }
-    //     }
-    //   },
-    //   {
-    //     $project: {
-    //       matchedTickets: 0
-    //     }
-    //   }
-    // ])
-    //     const ids = users.map(user => user.userId).filter(id => id !== 140779820);
-    //     console.log('ids', ids.length)
-    //     const success = [];
-    //     const fail = [];
-    //     const mapLink = `<a href="https://www.instagram.com/sverlovsk">@sverlovsk</a>`;
-    //     for (const id of ids) {
-    //       try {
-    //         await axios.post(`${config.tgApiUrl}/sendPhoto`, {
-    //           chat_id: id,
-    //           parse_mode: 'HTML',
-    //           photo: 'https://www.dropbox.com/scl/fi/pfxe9l923hal1imq5lhq9/what.jpg?rlkey=9vk13epfpfnont2jcjq90z8oi&raw=1',
-    //           caption: `Я к тебе с хорошей новостью - цены на билеты в Муйне снизились\nУспевай купить, пока не разобрали`,
-    //           reply_markup: {
-    //             inline_keyboard: [
-    //               [
-    //                 { text: "Новые цены на Стендап-концерт 06.03 20:00", url: 'https://t.me/sverlov_vietnam_2026_bot?startapp=SOURCE_SPLIT_PRICE-LOW-NOTICE_SEP_EVENT_SPLIT_6985e0b63677bfc5bc8757c3' },
-    //               ],
-    //               [
-    //                 { text: "Новые цены на Шоу-Импровизация 08.03 20:00", url: 'https://t.me/sverlov_vietnam_2026_bot?startapp=SOURCE_SPLIT_PRICE-LOW-NOTICE_SEP_EVENT_SPLIT_6985e0b63677bfc5bc8757c4' },
-    //               ]
-    //             ]
-    //           },
-    //         });
-    //         console.log('sent to ', id)
-    //         success.push(id)
-
-    //       } catch (error) {
-    //         console.log(error)
-    //         fail.push(id)
-    //       }
-    //     }
-    //     console.log('success', success.length)
-    //     console.log('fail', fail.length)
-
-    // await ticketsService.sendTickets({bookingId: 'OQUo6-HWAjPN-g'})
-    // await dataService.updateDocuments('user', {}, {$set: {sessionId: 'OldUser'}});
-
-    // const sources = ['AFISHA',
-    //   'BOT-AFISHA-RECALL',
-    //   'CONCERT',
-    //   'FACEBOOK',
-    //   'FLAERS',
-    //   'KAIFUSHNIKI',
-    //   'KVIZDA-2',
-    //   'KVIZDA-3',
-    //   'KVIZDATUE-LUDI',
-    //   'NHA-TRANG-EVENTS',
-    //   'NHA-TRANG-SOURCE-1',
-    //   'NHA-TRANG-SOURCE-2',
-    //   'NHA-TRANG-SOURCE-8',
-    //   'NHA-TRANG-TUSA',
-    //   'NORTH-PEOPLE',
-    //   'PRICE-LOW-NOTICE',
-    //   'RECALL',
-    //   'RECALL-2',
-    //   'RECALL-3',
-    //   'VIETHELPER',
-    //   'all-tour',
-    //   'mui-ne',
-    //   'mui_ne',
-    //   'muine',
-    //   'muine-facebook',
-    //   'muine-flaer-3',
-    //   'muine-flaer-5',
-    //   'muine-sticker',
-    //   'muine-tg-chat-1',
-    //   'muine-tg-chat-12',
-    //   'muine-tg-chat-14',
-    //   'muine-tg-chat-15',
-    //   'muine-tg-chat-16',
-    //   'muine-tg-chat-18',
-    //   'muine-tg-chat-19',
-    //   'muine-tg-chat-2',
-    //   'muine-tg-chat-22',
-    //   'muine-tg-chat-24',
-    //   'muine-tg-chat-3',
-    //   'muine-tg-chat-6',
-    //   'muine-tg-chat-7',
-    //   'muine-tg-chat-8',
-    //   'phu-quoc',
-    //   'phuquoc',
-    //   'test3',
-    //   'tour',
-    //   'unknown'
-    // ]
-    // const users = await dataService.getDocuments('user', {'path.0': "scroll reels"});
-    // for (const user of users) {
-    //   user.path = ['unknown', ...user.path];
-    //   await dataService.updateDocument('user', user)
-    // }
+    // console.log('success', success.length)
+    // console.log('fail', fail.length)
 
     res.status(200).send(true);
     return;
