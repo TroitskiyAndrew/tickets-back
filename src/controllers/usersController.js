7f0516b89c156657df0ea79647eb8faf0e75e5e2
AndreyTroitskiy <TroitskiyA@yandex.ru>
2026-03-14 11:49:18 +0700

    feat: spam

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -77,83 +77,69 @@ const findUsers = async (req, res) => {
 const sendMessage = async (req, res) => {
   try {
     const andrei = 480144364
-    // const user = { id: 480144364, first_name: 'Test', username:'alevtina_psychologist'  };
-    //  const userLink = `<a href="https://t.me/${user.username}">${user.first_name || user.username || 'Пользователь'}</a>`;
-    // await axios.post(`${config.tgApiUrl}/sendMessage`, {
-    //   chat_id: 480144364,
-    //   parse_mode: 'HTML',
-    //   text: `Сообщение от ${userLink}`,
-    // });
-    // await dataService.updateDocuments('event', {}, {$set: {entrance: [480144364,692369447,140779820]}})
 
+    const aggregation = [
+  {
+    $match: {
+      visits: "6984920943c748577e5f8705"
+    }
+  },
+  {
+    $lookup: {
+      from: "ticket",
+      let: { uid: "$userId" },
+      pipeline: [
+        {
+          $match: {
+            event: "6985e0b63677bfc5bc8757c6",
+            $expr: {
+              $eq: ["$userId", "$$uid"]
+            }
+          }
+        },
+        { $limit: 1 }
+      ],
+      as: "ticketMatch"
+    }
+  },
+  {
+    $match: {
+      ticketMatch: { $eq: [] }
+    }
+  }
+]
 
-    // const users = await dataService.aggregate('user', [
-    //   {
-    //     $match: {
-    //       visits: "6984920343c748577e5f8704"
-    //     }
-    //   },
-    //   {
-    //     $lookup: {
-    //       from: "ticket",
-    //       let: { uid: "$userId" },
-    //       pipeline: [
-    //         {
-    //           $match: {
-    //             $expr: {
-    //               $and: [
-    //                 { $eq: ["$userId", "$$uid"] },
-    //                 { $eq: ["$event", "6985e0b63677bfc5bc8757c3"] }
-    //               ]
-    //             }
-    //           }
-    //         },
-    //         { $limit: 1 }
-    //       ],
-    //       as: "tickets"
-    //     }
-    //   },
-    //   {
-    //     $match: {
-    //       tickets: { $eq: [] }
-    //     }
-    //   },
-    //   {
-    //     $project: {
-    //       tickets: 0
-    //     }
-    //   }
-    // ])
-    // const ids = users.map(user => user.userId).filter(id => id !== 140779820);
-    // console.log('ids', ids.length)
-    // const success = [];
-    // const fail = [];
-    // const mapLink = `<a href="https://www.instagram.com/sverlovsk">@sverlovsk</a>`;
-    // for (const id of ids) {
-    //   try {
-    //     await axios.post(`${config.tgApiUrl}/sendPhoto`, {
-    //       chat_id: id,
-    //       parse_mode: 'HTML',
-    //       photo: 'https://www.dropbox.com/scl/fi/apchcqxxw0q07cnj4ht4x/photo_2026-03-03_01-02-40.jpg?rlkey=rql3yov33n9xsz7age9y8gvlf&raw=1',
-    //       caption: `Чего ждем? Стендап концерт в Муйне уже сегодня, бери билеты пока не разобрали!`,
-    //       reply_markup: {
-    //         inline_keyboard: [
-    //           [
-    //             { text: "Посмотреть билеты", url: 'https://t.me/sverlov_vietnam_2026_bot?startapp=SOURCE_SPLIT_MUINE-RECALL_SEP_EVENT_SPLIT_6985e0b63677bfc5bc8757c3' },
-    //           ]
-    //         ]
-    //       },
-    //     });
-    //     console.log('sent to ', id)
-    //     success.push(id)
+    const users = await dataService.aggregate('user', aggregation)
+    const ids = users.map(user => user.userId).filter(id => id !== 140779820);
+    console.log('ids', ids.length)
+    const success = [];
+    const fail = [];
+    const mapLink = `<a href="https://www.instagram.com/sverlovsk">@sverlovsk</a>`;
+    for (const id of [andrei]) {
+      try {
+        await axios.post(`${config.tgApiUrl}/sendVideo`, {
+          chat_id: id,
+          parse_mode: 'HTML',
+          video: 'https://dl.dropboxusercontent.com/scl/fi/2cjt7dj8scrwty007yjoh/compressed.mp4?rlkey=cwfx8yuxbj8qu03wqiamxosf4&dl=1',
+          caption: `Не пропусти! Сегозня последнее шоу на Фукуоке!\nСтендап-импровизация с залом, где Дмитрий по желанию вызывает зрителей на сцену,\nобщается с ними и создает юморитическое шоу прямо у вас на глазах`,
+          reply_markup: {
+            inline_keyboard: [
+              [
+                { text: "Билеты со скидкой 20%", url: 'https://t.me/sverlov_vietnam_2026_bot?startapp=SOURCE_SPLIT_FUKUOK-LAST-CALL-DISCOUNT_SEP_EVENT_SPLIT_6985e0b63677bfc5bc8757c6_SEP_DISCOUNT_SPLIT_6985e0b63677bfc5bc8757c6_D_FUKUOK-LAST-CALL' },
+              ]
+            ]
+          },
+        });
+        console.log('sent to ', id)
+        success.push(id)
 
-    //   } catch (error) {
-    //     console.log(error)
-    //     fail.push(id)
-    //   }
-    // }
-    // console.log('success', success.length)
-    // console.log('fail', fail.length)
+      } catch (error) {
+        console.log(error)
+        fail.push(id)
+      }
+    }
+    console.log('success', success.length)
+    console.log('fail', fail.length)
 
     res.status(200).send(true);
     return;
